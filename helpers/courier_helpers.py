e58d1d8930707d8de3e839e6b9c72b00c2af6368
Sergei Karlashov <velanishkis@yandex.ru>
2026-05-06 20:14:09 +0300

    ne poymu gde moi kommentarrii v code nu ladno localno est yge horosho

--- helpers/courier_helpers.py
+++ helpers/courier_helpers.py
@@ -3,21 +3,21 @@ from helpers.generators import random_lower_string
 from data.payloads import COURIER_CREATE_TEMPLATE, COURIER_LOGIN_TEMPLATE
 
 @allure.step("Сгенерировать уникальный payload курьера")
-def build_unique_courier_payload():
-    payload = COURIER_CREATE_TEMPLATE.copy()
+def build_unique_courier_payload():  #генерируем курьера
+    payload = COURIER_CREATE_TEMPLATE.copy() # шаблон курьера потом заполяем случайностями
     payload["login"] = random_lower_string(10)
     payload["password"] = random_lower_string(10)
     payload["firstName"] = random_lower_string(10)
     return payload
 
-@allure.step("Собрать payload логина курьера")
+@allure.step("Собрать payload логина курьера") # собираем тело для логина курьера
 def build_courier_login_payload(login: str, password: str):
     payload = COURIER_LOGIN_TEMPLATE.copy()
     payload["login"] = login
     payload["password"] = password
     return payload
 
-@allure.step("Создать курьера и получить его id через логин")
+@allure.step("Создать курьера и получить его id через логин") # создаем логинисмся достсаем id
 def create_courier_and_get_id(courier_client):
     courier_payload = build_unique_courier_payload()
     create_resp = courier_client.create_courier(courier_payload)
